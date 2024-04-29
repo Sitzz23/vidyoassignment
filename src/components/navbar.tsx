537679779f4732779c79e08889842eacf8d910ae
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-29 12:37:35 +0530

    add: navbar and logo

---
+++ src/components/navbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-background/60 sticky top-0 z-10 flex h-16 items-center justify-center gap-10 border-b px-4 backdrop-blur-xl transition-all">
+      <Link href="/" className="flex items-center space-x-2">
+        {/* <span className="font-urban inline-block text-xl font-bold">
+          Vidyo.ai
+        </span> */}
+        <Image
+          src={"/logo.png"}
+          alt={"logo"}
+          width={100}
+          height={24}
+          className="aspect-[3.92]"
+        />
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
