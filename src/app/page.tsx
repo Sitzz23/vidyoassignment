e481c1a6bd7a42f871e944a3849a34e1113f4d38
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-29 15:08:59 +0530

    add: hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Balancer from "react-wrap-balancer";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center "></main>
+    <main className="flex min-h-screen flex-col items-center justify-center space-y-6 bg-[#F5F7F2]  lg:py-28">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
+          <Balancer>Save 10x time & effort with us</Balancer>
+        </h1>
+        <p className="leading-normal sm:text-xl sm:leading-8">
+          <Balancer>
+            Pay only for upload minutes. Download unlimited videos.
+          </Balancer>
+        </p>
+      </div>
+    </main>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,11 +5,8 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-background/60 sticky top-0 z-10 flex h-16 items-center justify-center gap-10 border-b px-4 backdrop-blur-xl transition-all">
+    <nav className="bg-background/60 sticky top-0 z-10 flex h-14 items-center justify-center gap-10 border-b px-4 backdrop-blur-xl transition-all">
       <Link href="/" className="flex items-center space-x-2">
-        {/* <span className="font-urban inline-block text-xl font-bold">
-          Vidyo.ai
-        </span> */}
         <Image
           src={"/logo.png"}
           alt={"logo"}
