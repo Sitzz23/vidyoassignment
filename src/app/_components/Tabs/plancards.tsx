c66839489a0566c2be66beb7eb799af9009c187d
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-30 00:51:15 +0530

    add:  final cards

--- src/app/_components/Tabs/plancards.tsx
+++ src/app/_components/Tabs/plancards.tsx
@@ -9,41 +9,60 @@ import { Button } from "~/components/ui/button";
 import data from "./CardData";
 import { PlanDuration } from "~/lib/interfaces/planduration.inerface";
 import { Separator } from "~/components/ui/separator";
+import { Badge } from "~/components/ui/badge";
 
 const PlanCards = ({ duration }: { duration: PlanDuration }) => {
+  const planCost = null;
   return (
     <div className="mt-6">
       {duration === PlanDuration.Monthly ? (
-        <div className="mx-auto grid max-w-sm grid-cols-1 items-start gap-4 px-8 md:grid-cols-2 lg:max-w-none lg:grid-cols-4">
-          {Object.keys(data.monthly).map((plan, index) => (
-            <div key={index}>
-              <Card className=" ">
-                <CardHeader>
-                  <CardTitle className="text-base font-semibold">
-                    {data.monthly[plan]?.title}
-                  </CardTitle>
-                  <div className="flex items-baseline gap-1">
-                    <CardTitle className="text-3xl font-bold">
-                      {plan === "free" ? "USD 0" : "USD 15"}
+        <div className="mx-auto grid max-w-sm grid-cols-1 items-start gap-4 md:grid-cols-2 lg:max-w-none lg:grid-cols-4">
+          {Object.keys(data.monthly)
+            // .slice(0, 3)
+            .map((plan, index) => (
+              <div key={index}>
+                <Card
+                  className={
+                    plan === "growth"
+                      ? "bg-[#252521] text-[#F6F6F2]"
+                      : "bg-white"
+                  }
+                >
+                  <CardHeader>
+                    <CardTitle
+                      className={`flex items-center gap-2 text-base font-medium tracking-[0.1px]`}
+                    >
+                      {data.monthly[plan]?.title}
+                      {plan === "growth" && (
+                        <Badge
+                          variant={"accent"}
+                          className="rounded-md text-xs"
+                        >
+                          Recommended
+                        </Badge>
+                      )}
                     </CardTitle>
-                    <CardDescription>/month</CardDescription>
-                  </div>
-                </CardHeader>
-                <CardContent className="mt-4">
-                  <Button variant={"default"} className="w-full" disabled>
-                    Start for free
-                  </Button>
-                </CardContent>
-                <div className="mb-4 px-6">
-                  <Separator />
-                </div>
-                <CardContent className="">
-                  <ul>
-                    {data.monthly[plan]?.features.map((feature, index) => (
-                      <li
-                        key={index}
-                        className="flex items-center justify-start gap-3 py-1"
+                    <div className="flex items-baseline gap-1">
+                      <CardTitle className="pt-2 text-4xl font-bold">
+                        USD&nbsp;
+                        {plan !== "custom"
+                          ? data.monthly[plan]?.cost
+                          : planCost}
+                      </CardTitle>
+                      <CardDescription>/month</CardDescription>
+                    </div>
+                  </CardHeader>
+                  <CardContent className="mt-4">
+                    {plan === "free" ? (
+                      <Button variant={"default"} className="w-full" disabled>
+                        Start for free
+                      </Button>
+                    ) : (
+                      <Button
+                        variant={plan === "growth" ? "default" : "outline"}
+                        className={`w-full ${plan === "growth" ? "bg-[#943DEC] text-[#F6F6F2]" : "text-zinc-950"}`}
                       >
+                        Start for free
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 16 16"
@@ -52,19 +71,44 @@ const PlanCards = ({ duration }: { duration: PlanDuration }) => {
                         >
                           <path
                             fillRule="evenodd"
-                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                            d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z"
                             clipRule="evenodd"
                           />
                         </svg>
+                      </Button>
+                    )}
+                  </CardContent>
+                  <div className="mb-4 px-6">
+                    <Separator />
+                  </div>
+                  <CardContent className="">
+                    <ul>
+                      {data.monthly[plan]?.features.map((feature, index) => (
+                        <li
+                          key={index}
+                          className="flex items-center justify-start gap-3 py-1"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
 
-                        <span>{feature}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </CardContent>
-              </Card>
-            </div>
-          ))}
+                          <span>{feature}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
         </div>
       ) : (
         <div>
--- src/app/_components/Tabs/tabplan.tsx
+++ src/app/_components/Tabs/tabplan.tsx
@@ -2,26 +2,33 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { Badge } from "~/components/ui/badge";
 import PlanCards from "./plancards";
 import { PlanDuration } from "~/lib/interfaces/planduration.inerface";
+import { Slider } from "~/components/ui/slider";
 
 const TabPlan = () => {
   return (
-    <Tabs defaultValue="monthly" className="w-full">
-      <TabsList className="mx-auto grid w-[400px] grid-cols-2">
-        <TabsTrigger value="monthly">Monthly</TabsTrigger>
-        <TabsTrigger value="yearly">
-          Yearly
-          <Badge className="ml-2" variant={"accent"}>
-            Upto 51% off
-          </Badge>
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="monthly" className="">
-        <PlanCards duration={PlanDuration.Monthly} />
-      </TabsContent>
-      <TabsContent value="yearly">
-        <PlanCards duration={PlanDuration.Yearly} />
-      </TabsContent>
-    </Tabs>
+    <div className="flex w-full flex-col gap-8">
+      <div className="flex items-center justify-between ">
+        <p>Suggesting an ideal plan for producing around 125 clips monthly</p>
+        <Slider defaultValue={[33]} max={100} step={1} className="w-[30vw]" />
+      </div>
+      <Tabs defaultValue="monthly" className="w-full">
+        <TabsList className="mx-auto grid w-[400px] grid-cols-2">
+          <TabsTrigger value="monthly">Monthly</TabsTrigger>
+          <TabsTrigger value="yearly">
+            Yearly
+            <Badge className="ml-2" variant={"accent"}>
+              Upto 51% off
+            </Badge>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="monthly" className="">
+          <PlanCards duration={PlanDuration.Monthly} />
+        </TabsContent>
+        <TabsContent value="yearly">
+          <PlanCards duration={PlanDuration.Yearly} />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 };
 
