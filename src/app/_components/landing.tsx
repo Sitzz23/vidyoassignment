e3649353d41b98f84bf2931b51c918b0fff3fc2b
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-30 02:06:20 +0530

    updated with for page

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -3,7 +3,7 @@ import TabPlan from "./Tabs/tabplan";
 
 const Landing = () => {
   return (
-    <div className="z-20 m-2 mx-auto flex min-h-screen w-[80%] flex-col items-center justify-start space-y-6 lg:py-28">
+    <div className="z-20 m-2 mx-auto flex min-h-screen w-[75%] flex-col items-center justify-start space-y-6 lg:py-28">
       <div className="container mb-2 flex flex-col items-center gap-5 text-center">
         <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
           <Balancer>Save 10x time & effort with us</Balancer>
