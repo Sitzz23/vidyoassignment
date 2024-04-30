44709b9dee47960dfcf030b97b93310039692a73
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-30 10:38:43 +0530

    accent button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        accent: "bg-[#b7f564] text-zinc-950 hover:bg-[#8ae215]",
       },
       size: {
         default: "h-10 px-4 py-2",
