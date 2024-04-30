7ac249a2a6b00eab84cc77421953a0519bcbd944
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-30 16:38:52 +0530

    state managed table visiblity

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -1,8 +1,15 @@
+"use client";
 import Balancer from "react-wrap-balancer";
 import TabPlan from "./Tabs/tabplan";
 import { FeatureTable } from "./Features/featureTable";
+import { Button } from "~/components/ui/button";
+import { useState } from "react";
 
 const Landing = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleIsOpen() {
+    setIsOpen((prevState) => !prevState);
+  }
   return (
     <div className="z-20 m-2 mx-auto flex min-h-screen w-[75%] flex-col items-center justify-start space-y-6 lg:py-28">
       <div className="container mb-2 flex flex-col items-center gap-5 text-center">
@@ -16,7 +23,36 @@ const Landing = () => {
         </p>
       </div>
       <TabPlan />
-      <FeatureTable />
+      <div className="flex w-full flex-col items-center gap-8 pt-4">
+        <Button
+          variant={"outline"}
+          className="w-2/12 bg-[#F5F7F2] hover:bg-[#FAFAF8]"
+          onClick={() => toggleIsOpen()}
+        >
+          View all features{" "}
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        </Button>
+        <div
+          className={`w-full transition-opacity duration-300 ${
+            isOpen ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          {isOpen && <FeatureTable />}
+        </div>
+      </div>
     </div>
   );
 };
